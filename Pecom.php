dec97e4556ded7570c770062a7fcb66f43189169
Melnikov Ruslan <melnikov@novator.angara.ru>
2017-09-22 12:22:38 +0800

    initial

---
+++ Pecom.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace mrssoft\pecom;
+
+use yii\base\Component;
+
+/**
+ * Public API for https://pecom.ru/business/developers/api_public/
+ */
+class Pecom extends Component
+{
+    /**
+     * Getting a list of delivery cities
+     * @return array
+     */
+    public function cityList()
+    {
+        $data = file_get_contents('http://www.pecom.ru/ru/calc/towns.php');
+        if ($data) {
+            return json_decode($data, true);
+        }
+
+        return null;
+    }
+
+    /**
+     * Calculating the cost of delivery
+     * @param array $params
+     */
+    public function calc(array $params)
+    {
+        $params = http_build_query($params);
+
+        $data = file_get_contents('http://calc.pecom.ru/bitrix/components/pecom/calc/ajax.php?' . $params);
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# yii2-pecom
+Yii2 component for https://pecom.ru/business/developers/api_public/
